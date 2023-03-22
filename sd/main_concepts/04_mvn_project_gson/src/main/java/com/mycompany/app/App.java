36391ed54241817c26ef20da7d96f51bf673df43
David Petrocelli <david.petrocelli@cloudhesive.com>
2023-03-21 21:50:24 -0300

    docker build + deployment

--- sd/main_concepts/04_mvn_project_gson/src/main/java/com/mycompany/app/App.java
+++ sd/main_concepts/04_mvn_project_gson/src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,13 @@
 package com.mycompany.app;
 
-/**
- * 
- * Hello world!
- */
-
- import com.google.gson.Gson;
- import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class App {
@@ -17,8 +18,21 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Server is starting");
         Gson gson = new Gson();
-        System.out.println("Server is 2");
+        System.out.println("Gson object manager is created");
 
+        Staff staff = new Staff();
+        staff.setName("seba");
+        staff.setAge(24);
+        staff.setPosition(new String[]{"estudiante", "desarrollador", "tester"});
+
+        String jsonStaff = gson.toJson(staff);
+        System.out.println ("STAFF OBJECT: "+jsonStaff);
+
+        Staff staff2 = gson.fromJson(jsonStaff, Staff.class);
+        staff.setName("facu1");
+        System.out.println ("STAFF OBJECT 2 -name: "+staff2.getName());
+        String jsonStaff2 = gson.toJson(staff);
+        System.out.println ("STAFF OBJECT 2 : "+jsonStaff2);
         //App application = new App();
 
     }
